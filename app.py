57790f383fe4817c477d1e51c97d9fa35f184306
Sudarshan Kadge <66057391+Sudarshan-Kadge@users.noreply.github.com>
2021-05-15 12:23:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+#IMporting Libraries
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+app = FLask(__name__)
+#Create Chatbot
+englishBot = ChatBot("Chatterbot", storage_adapter="Chatterbot.Storage.SQLStorageAdapter")
+trainer = ChatterBotCorpusTrainer(englishBot)
+trainer.train("chatterbot.corpus.english")  #Train the chatterbor for the English
+
+#define app routes
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/get")
+#Function for the boat response
+def get_bot_response():
+    userText=request.args.get('msg')
+    return str(englishBot.get_response(userText))
+
+if __name__ =="__main__":
+    app.run()
